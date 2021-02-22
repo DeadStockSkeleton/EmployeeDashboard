f75987381312e9da749ef0f7918dae6989f35f2a
Iyana Medlock <iyanamedlock@gmail.com>
2021-02-22 15:53:56 -0600

    added validation and fixed id

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const Employee = require("./lib/Employee");
 const Intern = require("./lib/Intern");
 const inquirer = require("./node_modules/inquirer/lib/inquirer.js");
 const fs = require("fs");
+const open = require('open');
 
 inquirer
   .prompt([
@@ -28,6 +29,12 @@ inquirer
       type: "input",
       message: "Office Number: ",
       name: "officeNumber",
+      validate: (answer) => {
+        if (answer.length === 0 || answer.ty) {
+          return false;
+        }
+        return true;
+      },
       when: (checked) => {
         if (checked.position.includes("Manager")) {
           return true;
@@ -39,6 +46,12 @@ inquirer
       type: "input",
       message: "Github Username: ",
       name: "github",
+      validate: (answer) => {
+        if (answer.length === 0) {
+          return false;
+        }
+        return true;
+      },
       when: (checked) => {
         if (checked.position.includes("Engineer")) {
           return true;
@@ -50,6 +63,12 @@ inquirer
       type: "input",
       message: "School/University: ",
       name: "school",
+      validate: (answer) => {
+        if (answer.length === 0) {
+          return false;
+        }
+        return true;
+      },
       when: (checked) => {
         if (checked.position.includes("Intern")) {
           return true;
@@ -59,167 +78,205 @@ inquirer
     },
   ])
   .then(function (data) {
-    let newMember;
-    let i;
-    let positions = [];
-    let members = [];
+    var newMember;
+    let i = 0;
+    let pop = [];
+    var members = [];
     let count = 0;
     let card;
     let html = "";
 
-    fs.readFile("members.json", function (err, res) {
+    fs.readFile("members.json", "utf8", function (err, res) {
       members = JSON.parse(res);
-      for (i = 1; i < members.length; i++) {
+      if (members.length > 0) {
+        for (i = 0; i < members.length; i++) {
+          switch (data.position) {
+            case "Manager":
+              newMember = new Manager(
+                data.name,
+                i + 1,
+                data.email,
+                data.officeNumber,
+                data.position
+              );
+
+              break;
+            case "Intern":
+              newMember = new Intern(
+                data.name,
+                i + 1,
+                data.email,
+                data.school,
+                data.position
+              );
+
+              break;
+            case "Engineer":
+              newMember = new Employee(
+                data.name,
+                i + 1,
+                data.email,
+                data.github,
+                data.position
+              );
+
+              break;
+          }
+        }
+      } else {
         switch (data.position) {
           case "Manager":
             newMember = new Manager(
               data.name,
-              i,
+              i++,
               data.email,
               data.officeNumber,
               data.position
             );
 
+            break;
           case "Intern":
             newMember = new Intern(
               data.name,
-              i,
+              i++,
               data.email,
               data.school,
               data.position
             );
 
+            break;
           case "Engineer":
             newMember = new Employee(
               data.name,
-              i,
+              i++,
               data.email,
               data.github,
               data.position
             );
 
+            break;
         }
-        
-        
       }
-    
-         members.push(newMember); 
-      
-      
-      fs.writeFileSync("members.json", JSON.stringify(members), function (err) {
-        if (err){
-            console.error("Error")
-        }
-        else{
-           console.log("saved"); 
-        }
-        
-      })
 
-      fs.readFile('members.json', function (err, r){
-          let pop = JSON.parse(r);
+      members.push(newMember);
+      fs.writeFile("members.json", JSON.stringify(members), function (err) {
+        if (err) throw err;
+        console.log("saved");
+        try {
+          fs.readFile("members.json", "utf8", function (err, res) {
+            pop = JSON.parse(res);
+            for (let x = 0; x < pop.length; x++) {
+              switch (pop[x].role) {
+                case "Manager":
+                  card += `<div class="card m-3" style='width:300px'>
+              <div class="card-header">
+                  <span>${
+                    pop[x].name.charAt(0).toUpperCase() + pop[x].name.slice(1)
+                  }</span> | <small class="text-muted">${pop[x].role}</small>
+              </div>
+              <div class="card-body">
+                  <ul class="list-group list-group-flush border rounded">
+                      <li class="list-group-item">ID: ${pop[x].id}</li>
+                      <li class="list-group-item">Email: ${pop[x].email} </li>
+                      <li class="list-group-item">Office Number: ${
+                        pop[x].officeNumber
+                      }</li>
+                  </ul>
+                    </div>
+                </div>`;
+                  break;
+                case "Intern":
+                  card += `<div class="card m-3" style='width:300px'>
+              <div class="card-header">
+                  <span>${
+                    pop[x].name.charAt(0).toUpperCase() + pop[x].name.slice(1)
+                  }</span> | <small class="text-muted">${pop[x].role}</small>
+              </div>
+              <div class="card-body">
+                  <ul class="list-group list-group-flush border rounded">
+                      <li class="list-group-item">ID: ${pop[x].id}</li>
+                      <li class="list-group-item">Email: ${pop[x].email} </li>
+                      <li class="list-group-item">School: ${pop[x].school}</li>
+                  </ul>
+                    </div>
+                </div>`;
+                  break;
+                case "Engineer":
+                  card += `<div class="card m-3" style='width:300px'>
+              <div class="card-header">
+                  <span>${
+                    pop[x].name.charAt(0).toUpperCase() + pop[x].name.slice(1)
+                  }</span> | <small class="text-muted">${pop[x].role}</small>
+              </div>
+              <div class="card-body">
+                  <ul class="list-group list-group-flush border rounded">
+                      <li class="list-group-item">ID: ${pop[x].id}</li>
+                      <li class="list-group-item">Email: ${pop[x].email} </li>
+                      <li class="list-group-item">Github:<a href='https://github.com/${
+                        pop[x].github
+                      }' >${pop[x].github}</a></li>
+                  </ul>
+                    </div>
+                </div>`;
+                  break;
+              }
 
-          for (let x = 0; x < pop.length; x++) {
-            switch (pop[x].role) {
-              case "Manager":
-                card += `<div class="card m-3" style='width:300px'>
-                  <div class="card-header">
-                      <span>${
-                        pop[x].name.charAt(0).toUpperCase() + pop[x].name.slice(1)
-                      }</span> | <small class="text-muted">${
-                        pop[x].role
-                      }</small>
-                  </div>
-                  <div class="card-body">
-                      <ul class="list-group list-group-flush border rounded">
-                          <li class="list-group-item">ID: ${pop[x].id}</li>
-                          <li class="list-group-item">Email: ${pop[x].email} </li>
-                          <li class="list-group-item">Office Number: ${
-                            pop[x].officeNumber
-                          }</li>
-                      </ul>
+              try {
+                html += card;
+              } finally {
+                (async () => {
+                   let newHtml = await new Promise((resolve, reject) =>{
+                    resolve(`<!DOCTYPE html>
+                    <html>
+                    
+                    <head>
+                        <meta charset="utf-8">
+                        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+                        <title></title>
+                        <meta name="description" content="">
+                        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet"
+                            integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
+                        <meta name="viewport" content="width=device-width, initial-scale=1">
+                        <link rel="stylesheet" href="./assets/style.css">
+                    </head>
+                    
+                    <body class='bg-light'>
+                        <nav class="navbar navbar-light bg-light border-bottom">
+                            <div class="container-fluid">
+                                <span class="navbar-brand">
+                                    Team<b class="text-secondary">Dashboard</b>
+                                </span>
+                            </div>
+                        </nav>
+                        <div class="container-fluid col-md-12">
+                            <div class="row align-items-start">
+                                <div id="container" class="col-md-12 p-5 d-flex">
+                                <div class="col-md-12 d-flex">${card}<br></div>  
+                                </div>
+                            </div>
+                    
                         </div>
-                    </div>`;
-                break;
-              case "Intern":
-                card += `<div class="card m-3" style='width:300px'>
-                  <div class="card-header">
-                      <span>${
-                        pop[x].name.charAt(0).toUpperCase() + pop[x].name.slice(1)
-                      }</span> | <small class="text-muted">${
-                        pop[x].role
-                      }</small>
-                  </div>
-                  <div class="card-body">
-                      <ul class="list-group list-group-flush border rounded">
-                          <li class="list-group-item">ID: ${pop[x].id}</li>
-                          <li class="list-group-item">Email: ${pop[x].email} </li>
-                          <li class="list-group-item">School: ${pop[x].school}</li>
-                      </ul>
-                        </div>
-                    </div>`;
-                break;
-              case "Engineer":
-                card += `<div class="card m-3" style='width:300px'>
-                  <div class="card-header">
-                      <span>${
-                        pop[x].name.charAt(0).toUpperCase() + pop[x].name.slice(1)
-                      }</span> | <small class="text-muted">${
-                  pop[x].role
-                }</small>
-                  </div>
-                  <div class="card-body">
-                      <ul class="list-group list-group-flush border rounded">
-                          <li class="list-group-item">ID: ${pop[x].id}</li>
-                          <li class="list-group-item">Email: ${pop[x].email} </li>
-                          <li class="list-group-item">Github:<a href='https://github.com/${
-                            pop[x].github
-                          }' >${pop[x].github}</a></li>
-                      </ul>
-                        </div>
-                    </div>`;
-                break;
+                    
+                        <script src="index.js" async defer></script>
+                    </body>
+                    
+                    </html>`)
+                  })
+
+                  let result = await newHtml
+
+                  fs.writeFileSync('index.html', result);
+                })
+                
+              }
             }
-            html = card;
-          }
-          fs.writeFileSync(
-            "index.html",
-            `<!DOCTYPE html>
-            <html>
+          });
+        } finally {
+          (async () => {
             
-            <head>
-                <meta charset="utf-8">
-                <meta http-equiv="X-UA-Compatible" content="IE=edge">
-                <title></title>
-                <meta name="description" content="">
-                <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet"
-                    integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
-                <meta name="viewport" content="width=device-width, initial-scale=1">
-                <link rel="stylesheet" href="./assets/style.css">
-            </head>
-            
-            <body class='bg-light'>
-                <nav class="navbar navbar-light bg-light border-bottom">
-                    <div class="container-fluid">
-                        <span class="navbar-brand">
-                            Team<b class="text-secondary">Dashboard</b>
-                        </span>
-                    </div>
-                </nav>
-                <div class="container-fluid d-inline">
-                    <div class="row align-items-start">
-                        <div id="container" class="col-md-12 p-5 d-flex">
-                            ${html}
-                        </div>
-                    </div>
-            
-                </div>
-            
-                <script src="index.js" async defer></script>
-            </body>
-            
-            </html>`
-          );
-      })
+            await open('index.html', {"wait": true });
+    })();
+        }
+      });
     });
   });
